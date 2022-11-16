55b94f7096acdf20c26bba30560348f08e614e31
Sergey <46376475+JustSergey@users.noreply.github.com>
2022-11-16 15:39:28 +0300

    Должно работать

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const gameName = "SHORT_NAME_YOUR_GAME";
 const queries = {};
 
 server.use(express.static(path.join(__dirname, 'public')));
-
-bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
+bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "This bot implements a T-Rex jumping game. Say /game if you want to play."));
 bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
 
 bot.on("callback_query", function (query) {
@@ -18,7 +17,7 @@ bot.on("callback_query", function (query) {
     bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
   } else {
     queries[query.id] = query;
-    let gameurl = "https://YOUR_URL_HERE/index.html?  id="+query.id;
+    let gameurl = "http://127.0.0.1/index.html?  id="+query.id;
     bot.answerCallbackQuery({
       callback_query_id: query.id,
       url: gameurl
